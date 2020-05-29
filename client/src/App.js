2293be108bb164c0f1160f9d5ae1a0a52cabd5bc
Abram Flansburg <abeflansburg@gmail.com>
2020-05-29 16:11:14 -0500

    can upload and convert and download with click

--- client/src/App.js
+++ client/src/App.js
@@ -1,52 +1,80 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, PureComponent } from 'react';
 import './App.css';
 
 function App() {
-  const [fileBuffer, setFileBuffer] = useState();
-  const [convertedImage, setConvertedImage] = useState();
-
+  const [fileBuffer, setFileBuffer] = useState(null);
+  const [convertedImage, setConvertedImage] = useState(null);
   useEffect(() => {
-    fetchConvertedImage(fileBuffer);
-  }, fileBuffer);
+    if (fileBuffer) {
+      console.log('fetching');
+      fetchConvertedImage(fileBuffer);
+    }
+  }, [fileBuffer]);
 
-  const handleFileInputChange = async (event) => {
-    event.preventDefault();
-    const buffer = await fileToBase64(event.target.files[0]);
-    setFileBuffer(buffer);
+  const handleFileInputChange = (event) => {
+    fileToBase64(event.target.files[0]);
   };
 
-  const fileToBase64 = (file) => {
+  function fileToBase64(file) {
     let fileReader;
 
     const handleFileReadEnd = (e) => {
       const content = fileReader.result;
-      console.log(content);
+      setFileBuffer(content);
     };
     fileReader = new FileReader();
     fileReader.onloadend = handleFileReadEnd;
     fileReader.readAsArrayBuffer(file);
-  };
+  }
+
+  function bufferToBase64(arrayBuffer) {
+    return btoa(String.fromCharCode(...new Uint8Array(arrayBuffer)));
+  }
 
   const fetchConvertedImage = async () => {
-    const res = await fetch(
-      'http://127.0.0.1:5000/image_conversion/convert/to/png',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/octet-stream',
-        },
-        body: fileBuffer,
-      }
-    );
-
-    console.log(res);
+    fetch('http://127.0.0.1:5000/image_conversion/convert/to/png', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/octet-stream',
+      },
+      body: fileBuffer,
+    })
+      .then((response) => {
+        const reader = response.body.getReader();
+        return new ReadableStream({
+          start(controller) {
+            return pump();
+            function pump() {
+              return reader.read().then(({ done, value }) => {
+                if (done) {
+                  controller.close();
+                  return;
+                }
+                controller.enqueue(value);
+                return pump();
+              });
+            }
+          },
+        });
+      })
+      .then((stream) => new Response(stream))
+      .then((response) => response.blob())
+      .then((blob) => URL.createObjectURL(blob))
+      .then((url) => setConvertedImage(url))
+      .catch((err) => {
+        throw new Error(err);
+      });
   };
 
   return (
     <div className="App">
       <h5> Right now can convert any image to PNG - fires on select</h5>
       <input type="file" onChange={handleFileInputChange}></input>
-      {fileBuffer && <p>{fileBuffer.toString()}</p>}
+      {convertedImage && (
+        <a href={`${convertedImage}`} download>
+          <img src={convertedImage} alt="converted png" />
+        </a>
+      )}
     </div>
   );
 }
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,14 @@
 import express, { Request, Response, NextFunction } from 'express';
-// import * as domainCheckRoute from './api/routes/domainCheck';
+import cors from 'cors';
 const imageConvertRoutes = require('./api/routes/imageConvert');
 
 const app: express.Application = express();
 
+app.use(cors());
+
 app.use((req: Request, res: Response, next: NextFunction) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'DELETE, POST, GET, OPTIONS');
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
